142a41d82e5684ff8cbcff8d8b41bc7bdc87fd8a
bobcermak <bohuslavcermak62@gmail.com>
2024-11-23 22:24:06 +0100

    update

--- script.js
+++ script.js
@@ -5,5 +5,4 @@ function toggleMenu() {
     hamburgerMenu.style.right = hamburgerMenu.style.right === '0px' ? '-100%' : '0';
     const fixedIcon = document.querySelector('.page__nav--menu-icon.close-icon');
     fixedIcon.classList.toggle('active');
-
 }
