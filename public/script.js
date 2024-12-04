5b250a7383d77c66b87cf625b9661d01d6b4a69c
bobcermak <bohuslavcermak62@gmail.com>
2024-12-04 18:39:29 +0100

    hotovo!!!

--- public/script.js
+++ public/script.js
@@ -1,11 +1,24 @@
 //Header
 
+//Side-bar
+
 function toggleMenu() {
     const hamburgerMenu = document.querySelector('.page__nav__list-hamburger');
-    hamburgerMenu.style.right = hamburgerMenu.style.right === '0px' ? '-100%' : '0';
     const fixedIcon = document.querySelector('.page__nav--menu-icon.close-icon');
+    hamburgerMenu.style.right = hamburgerMenu.style.right === '0px' ? '-100%' : '0';
     fixedIcon.classList.toggle('active');
 }
+function closeMenuOnClick() {
+    const hamburgerMenu = document.querySelector('.page__nav__list-hamburger');
+    const fixedIcon = document.querySelector('.page__nav--menu-icon.close-icon');
+    hamburgerMenu.style.right = '-100%';
+    fixedIcon.classList.remove('active');
+}
+document.querySelectorAll('.page__nav__list-hamburger a').forEach(link => {
+    link.addEventListener('click', closeMenuOnClick);
+});
+
+//Color
 
 const headerLinks = document.querySelectorAll('header a');
 headerLinks.forEach(link => {
@@ -13,6 +26,3 @@ headerLinks.forEach(link => {
         link.style.color = '#FE8C01';
     }
 });
-
-//!Animation
-
